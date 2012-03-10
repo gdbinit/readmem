0c93694cc1bf089cb322e8421e4696072a3f9c91
fG <reverser@put.as>
2012-03-10 23:54:41 +0000

    Fix the columns output and display memory protection flags

--- readmem/main.c
+++ readmem/main.c
@@ -10,6 +10,7 @@
  * gcc -Wall -o readkmem readkmem.c
  *
  * v0.1 - Initial version
+ * v0.2 - Fix the columns output and display memory protection flags
  *
  * Check http://246tnt.com/iPhone/ for iOS Entitlements reference
  *
@@ -30,39 +31,69 @@
 #if !defined (__arm__)
 #include <mach/mach_vm.h>
 #endif
+#include <mach/vm_region.h>
+#include <mach/vm_map.h>
 
 #define DEBUG 1
-#define VERSION "0.1"
+#define VERSION "0.2"
 
 #define x86 0
 #define x64	1
 
 #define MAX_SIZE 50000000
 
+// prototypes
 static void header(void);
-#if defined (__arm__)
-static void readmem(vm_offset_t *buffer, vm_address_t address, vm_size_t size, pid_t pid);
-#else
-static void readmem(mach_vm_offset_t *buffer, mach_vm_address_t address, mach_vm_size_t size, pid_t pid);
-#endif
 static void usage(void);
+static void get_protection(vm_prot_t protection, char *prot);
+
+#if defined (__arm__)
+static void readmem(vm_offset_t *buffer, vm_address_t address, vm_size_t size, pid_t pid, vm_region_basic_info_data_64_t *info);
+#else
+static void readmem(mach_vm_offset_t *buffer, mach_vm_address_t address, mach_vm_size_t size, pid_t pid, vm_region_basic_info_data_64_t *info);
+#endif
+
+// for iOS
+#if defined (__arm__)
+extern kern_return_t mach_vm_region
+(
+ vm_map_t target_task,
+ mach_vm_address_t *address,
+ mach_vm_size_t *size,
+ vm_region_flavor_t flavor,
+ vm_region_info_t info,
+ mach_msg_type_number_t *infoCnt,
+ mach_port_t *object_name
+ );
+#endif
 
 #if defined (__arm__)
 static void 
-readmem(vm_offset_t *buffer, vm_address_t address, vm_size_t size, pid_t pid)
+readmem(vm_offset_t *buffer, vm_address_t address, vm_size_t size, pid_t pid, vm_region_basic_info_data_64_t *info)
 #else
 static void 
-readmem(mach_vm_offset_t *buffer, mach_vm_address_t address, mach_vm_size_t size, pid_t pid)
+readmem(mach_vm_offset_t *buffer, mach_vm_address_t address, mach_vm_size_t size, pid_t pid, vm_region_basic_info_data_64_t *info)
 #endif
 {
     // get task for pid
     vm_map_t port;
 
     kern_return_t kr;
-    
+
     if (task_for_pid(mach_task_self(), pid, &port))
     {
         fprintf(stderr, "[ERROR] Can't execute task_for_pid! Do you have the right permissions/entitlements?\n");
+        exit(1);
+    }
+    
+    mach_msg_type_number_t info_cnt = sizeof (vm_region_basic_info_data_64_t);
+    mach_port_t object_name;
+    mach_vm_size_t size_info;
+    mach_vm_address_t address_info = address;
+    kr = mach_vm_region(port, &address_info, &size_info, VM_REGION_BASIC_INFO_64, (vm_region_info_t)info, &info_cnt, &object_name);
+    if (kr)
+    {
+        fprintf(stderr, "[ERROR] mach_vm_region failed with error %d\n", (int)kr);
         exit(1);
     }
 
@@ -81,6 +112,18 @@ readmem(mach_vm_offset_t *buffer, mach_vm_address_t address, mach_vm_size_t size
     }
 }
 
+/*
+ * get an ascii representation of memory protection
+ */
+static void 
+get_protection(vm_prot_t protection, char *prot)
+{
+    prot[0] = protection & 1 ? 'r' : '-';
+    prot[1] = protection & (1 << 1) ? 'w' : '-';
+    prot[2] = protection & (1 << 2) ? 'x' : '-';
+    prot[3] = '\0';
+}
+
 static void
 usage(void)
 {
@@ -93,9 +136,10 @@ usage(void)
 static void
 header(void)
 {
-	fprintf(stderr,"Readmem v%s - (c) fG!\n",VERSION);
-	fprintf(stderr,"-----------------------\n");
+	fprintf(stderr,"[ Readmem v%s - (c) fG! ]\n",VERSION);
+	fprintf(stderr,"--------------------------\n");
 }
+
 
 int 
 main(int argc, char ** argv)
@@ -121,7 +165,6 @@ main(int argc, char ** argv)
     uint64_t address = 0;
 #endif
 
-	
 	// process command line options
 	while ((c = getopt_long (argc, argv, "a:s:o:p:", long_options, &option_index)) != -1)
 	{
@@ -155,7 +198,7 @@ main(int argc, char ** argv)
 	
 	header();
 
-	if (argc < 6)
+	if (argc < 7)
 	{
 		usage();
 	}
@@ -183,11 +226,12 @@ main(int argc, char ** argv)
 		}
 	}
 	
+    vm_region_basic_info_data_64_t region_info;
 	// read memory
 #if defined (__arm__)
-    readmem((vm_offset_t*)readbuffer, address, size, pid);
+    readmem((vm_offset_t*)readbuffer, address, size, pid, &region_info);
 #else
-    readmem((mach_vm_offset_t*)readbuffer, address, size, pid);
+    readmem((mach_vm_offset_t*)readbuffer, address, size, pid, &region_info);
 #endif
 	
     // dump to file
@@ -207,7 +251,15 @@ main(int argc, char ** argv)
         int x = 0;
         int z = 0;
         int linelength = 0;
-		printf("Memory hex dump @ %p:\n\n", (void*)address);
+		
+        // retrieve memory protection for the region of the starting address
+        // CAVEAT: it will be incorrect if dumping size includes more than one region
+        //         but we can't get protection per page
+        char current_protection[4]; 
+        char maximum_protection[4];
+        get_protection(region_info.protection, current_protection);
+        get_protection(region_info.max_protection, maximum_protection);
+        printf("Memory protection: %s/%s\n\n", current_protection, maximum_protection);
 		// 16 columns
 		while (i < size)
 		{
@@ -238,3 +290,4 @@ main(int argc, char ** argv)
     free(readbuffer);
 	return 0;
 }
+
